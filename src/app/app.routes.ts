7a0630f437f5018795de400492563b03e3ea06e8
ponpandiyan <ponpandiyan.s4787@gmail.com>
2024-06-01 17:57:17 +0530

    setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PartyListComponent } from './pages/party-list/party-list.component';
+import { PartyFormComponent } from './pages/party-form/party-form.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'party-list', component: PartyListComponent},
+    {path: 'party-form', component: PartyFormComponent},
+    {path: '', component: HomeComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
