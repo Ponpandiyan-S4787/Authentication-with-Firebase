1026aad191f56c53db4e534bd5d435481a6d0a16
ponpandiyan <ponpandiyan.s4787@gmail.com>
2024-06-01 19:27:36 +0530

    add nav

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pms-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
