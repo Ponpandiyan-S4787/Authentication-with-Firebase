72ed03bfd39680d82010b3092a679b76e2f3f2ae
ponpandiyan <ponpandiyan.s4787@gmail.com>
2024-06-03 22:50:03 +0530

    configuration of btn comp

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,16 +1,18 @@
-import { CommonModule, NgStyle } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MaterialsModule } from '../../modules/materials/materials.module';
 import {
   ControlValueAccessor,
   FormsModule,
   NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
 } from '@angular/forms';
+import { formControls } from '../../interfaces/forms-interface';
 
 @Component({
   selector: 'pms-input',
   standalone: true,
-  imports: [CommonModule, MaterialsModule, FormsModule, NgStyle],
+  imports: [CommonModule, MaterialsModule, FormsModule, ReactiveFormsModule],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -25,7 +27,8 @@ export class InputComponent implements ControlValueAccessor {
   @Input() label!: string;
   @Input() type!: string;
   @Input() placeholder!: string;
-  @Input() style!: any;
+  @Input() style!: object;
+  formControls = formControls;
   onChange: Function = () => {};
   onTouch: Function = () => {};
   set value(val: string) {
